a027f4b36fac86abfff5cd73739de817fcb18461
kunalprakash1309 <kunalprakash1309@gmail.com>
2022-06-12 14:21:30 +0530

    Added graphQl in place of categories context

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,24 +1,47 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+// gql => query which we made in graphql playground
+import { gql, useQuery} from '@apollo/client'
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
+  const { loading, error, data } = useQuery(COLLECTIONS)
 
-    getCategoriesMap();
-  }, []);
+  useEffect(() =>{
+    if(data) {
+      const { collections } = data;
 
-  const value = { categoriesMap };
+      const collectionsMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc
+      }, {})
+
+      setCategoriesMap(collectionsMap)
+    }
+  }, [data])
+
+  const value = { categoriesMap, loading, error };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
@@ -9,19 +10,27 @@ import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
 
+// It will register our app to server
+const client = new ApolloClient({
+  uri: 'https://crwn-clothing.com/',  // address of server
+  cache: new InMemoryCache()  // Cache memory for any previous query
+})
+
 const rootElement = document.getElementById('root');
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   rootElement
 );
